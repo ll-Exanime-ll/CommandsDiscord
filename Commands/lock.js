311706b8e28651956207a209462daf90ad00df4f
Exánime <60265869+Exanime0027@users.noreply.github.com>
2021-04-08 22:29:21 +0200

    Create lock.js

---
+++ Commands/lock.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: "lock",
+  category: "Moderation",
+  description: "Block channels",
+  run: async (client, message, args) => {
+  
+const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+        if (args[0] === 'on') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: false
+                }).then(() => {
+                    channel.setName(channel.name += `🔒`)
+                })
+            })
+            return message.channel.send('locked all channels');
+        } else if (args[0] === 'off') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: true
+                }).then(() => {
+                        channel.setName(channel.name.replace('🔒', ''))
+                    }
+                )
+            })
+            return message.channel.send('unlocked all channels')
+        }
+    }
+}
