751639ac8ba950d0f775396fe41210204e2c3e66
Exánime <60265869+Exanime0027@users.noreply.github.com>
2021-04-08 20:48:53 +0200

    Update NewSuggestion.js

--- SuggestionBasic/NewSuggestion.js
+++ SuggestionBasic/NewSuggestion.js
@@ -6,14 +6,14 @@ const client = new Client({
 
 module.exports = {
   name: "suggestion",
-  aliases: ["sugerencia"], //If you don't want any aliases you can delete this line
+  aliases: ["sugerencia"], //If you don't want any aliases you can delete this line.
   category: "Suggestions",
   description: "Suggestions",
   run: async (client, message, args) => { 
     const user = message.author.tag;
     let texto = args.join(" ");
     if (!texto) return message.delete();
-    let canal = client.channels.resolve('id') //ID of the channel you want the suggestion to be sent  
+    let canal = client.channels.resolve('id') //ID of the channel you want the suggestion to be sent  .
     const embedDatos = new Discord.MessageEmbed() 
     .setTitle("New Suggestion")
     .setDescription(texto)
