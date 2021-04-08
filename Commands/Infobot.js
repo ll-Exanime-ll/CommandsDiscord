8eddf53934553d9efbe1d0377521bffe64a29ee6
Exánime <60265869+Exanime0027@users.noreply.github.com>
2021-04-08 21:08:14 +0200

    Create Infobot.js

---
+++ Commands/Infobot.js
@@ -0,0 +1,49 @@
+  const { MessageEmbed } = require('discord.js')
+const os = require('os')
+
+module.exports = {
+  name: "infobot",
+  category: "Information",
+  description: "Displays bot information",
+  run: async (client, message, args) => {
+        const embed = new MessageEmbed()
+            .setThumbnail(client.user.displayAvatarURL())
+            .setTitle('Bot Stats')
+            .setColor('#000000')
+            .addFields(
+                {
+                    name: '🌐 Servers',
+                    value: `Serving ${client.guilds.cache.size} servers.`,
+                    inline: true
+                },
+                {
+                    name: '📺 Channels',
+                    value: `Serving ${client.channels.cache.size} channels.`,
+                    inline: true
+                },
+                {
+                    name: '👥 Server Users',
+                    value: `Serving ${client.users.cache.size}`,
+                    inline: true
+                },
+                {
+                    name: '⏳ Ping',
+                    value: `${Math.round(client.ws.ping)}ms`,
+                    inline: true
+                },
+                {
+                    name: '🗓️ Join Date',
+                    value: client.user.createdAt,
+                    inline: true
+                },
+                {
+                    name: '🛈 Server Info',
+                    value: `Cores: ${os.cpus().length}`,
+                    inline: true
+                }
+            )
+            .setFooter(`Created By: ${message.author.tag}`, message.author.displayAvatarURL())
+
+        await message.channel.send(embed)
+    }
+}
