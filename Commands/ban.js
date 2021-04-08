854911e006aca5c494ca51a07b62a8d3a3802e54
Exánime <60265869+Exanime0027@users.noreply.github.com>
2021-04-08 20:58:21 +0200

    Create ban.js

---
+++ Commands/ban.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "ban",
+  aliases: ["banear"],
+  category: "Moderation",
+  description: "Ban users",
+  run: async (client, message, args) => {
+        if (!message.member.hasPermission('BAN_MEMBERS')) {
+            return message.channel.send(`You are unable to ban members`)
+        }
+        if (!args[0]) {
+            return message.channel.send(`Please mention a user!`)
+        }
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+
+        try {
+            await member.ban();
+            await message.channel.send(`${member} has been banned!`)
+        } catch (e) {
+            return message.channel.send(`User is not in the server!`)
+        }
+
+    }
+}
