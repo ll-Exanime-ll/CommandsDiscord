ac6e4b9cbf87be4386eddf3316f1b5eccf0985f2
Exánime <60265869+ll-Exanime-ll@users.noreply.github.com>
2021-04-10 15:15:24 +0200

    Delete WelcomePrivate.js

--- 'server.js' or 'index.js'/WelcomePrivate.js
+++
@@ -1,7 +0,0 @@
-client.on("guildMemberAdd", member => {
-  member.send(
-    `> Hey <@${member.user.id}>, welcome to the server **${member.guild.name}**, Have fun!`
-  );
-});
-
-//Welcomes in private
